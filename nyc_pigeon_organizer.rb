4e6da213f7b4859d6e9dd5df455273291f44bf88
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-15 18:48:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,49 @@
-def nyc_pigeon_organizer(data)
-  listed_by_name = {}
-  data.each do |atributes, value| # color/gender etc and its value
-  value.each do |desription, names| # grey/male and its value
-  names.each do |name| #array of names
-  if listed_by_name.has_key?(name)
-    if listed_by_name[name].has_key?(atributes)
-      listed_by_name[name][atributes] << desription.to_s
-    else
-      listed_by_name[name][atributes] = [desription.to_s]
-    end
-    else
-    listed_by_name[name] = {atributes => [desription.to_s]}
+require "pry"
+
+def nyc_pigeon_organizer (pigeon_data)
+  new_data = {}
+  pigeon_data.each do |atributes,information|
+    information.each do |discription,name|
+      #binding.pry
+      if new_data.has_key?(name)
+        if new_data[name]has_key?(atributes)
+          
+          new_data[name][atributes] << information.to_s
+        
+      else 
+        new_data[name][atributes] = [information.to_s]
+        
+      end
+    else new_data[name] = {atributes => [information.to_s]}
   end
 end
+  end
+  end
+  return new_data
 end
-end
-return listed_by_name
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
